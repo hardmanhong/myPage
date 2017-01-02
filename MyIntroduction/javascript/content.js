af8ea44e649f8babb2c9a3e847e79e3f4b36d2fc
hardmanhong <hardmanhong@gmail.com>
2017-01-02 20:13:24 +0800

    个人简历网站

--- MyIntroduction/javascript/content.js
+++ MyIntroduction/javascript/content.js
@@ -21,10 +21,10 @@ getTime();
 //切换主题
 var changeTheme = document.getElementsByClassName("change-theme")[0];
 var content = document.getElementById("content");
+var body = document.querySelector("body");
 var isChange = false;
 changeTheme.addEventListener("click", function() {
-    content.style.backgroundColor = isChange == false ? "#151518" : "";
-    content.style.color = isChange == false ? "#aaa" : "";
+    body.className = isChange == false ? "theme" : "";
     isChange = !isChange;
 })
 
@@ -32,26 +32,43 @@ changeTheme.addEventListener("click", function() {
 var title = document.getElementsByClassName("title");
 var arrow = document.getElementsByClassName("arrow");
 var showDiv = document.getElementsByClassName("show");
-// console.log(title);
-// console.log(show);
 for(var i=0;i<title.length;i++){
+    title[i].index = i;
+    title[i].isShow = false;
     title[i].addEventListener("click",function () {
-        var text = this.innerText;
-        show(text);
+        show(this);
+        this.isShow = !this.isShow;
     });
 }
-
-function show (text) {
-    var text = text;
-    switch (text) {
-        case "学习":
-            console.log("学习");
-            break;
-        case "个人作品 —— 点击图片预览":
-            console.log("个人作品 —— 点击图片预览");
-            break;
-        case "爱好":
-            console.log("爱好");
-            break;
-    }
+function show (whichTitle) {
+    var index = whichTitle.index;
+    var isShow = whichTitle.isShow;
+    var img = title[index].getElementsByTagName("img")[0];
+    var downSrc = img.src.replace(/up/,"down");
+    var upSrc = img.src.replace(/down/,"up");
+    clearInterval(up);
+    clearInterval(down);
+    if(isShow){//isShow为true 点击展开
+        showDiv[index].style.display = "block";
+        var showTime = 0;
+        var down = setInterval(function () {
+        showTime += 10;
+        showDiv[index].style.opacity = showTime/100;
+        if(showTime >= 100) {
+            img.src = downSrc;
+            clearInterval(down);
+        }
+        },50);
+    }else {//isShow为false 点击收起
+        var showTime = 100;
+        var up = setInterval(function () {
+        showTime -= 10;
+        showDiv[index].style.opacity = showTime/100;
+        if(showTime <= 0) {
+            img.src = upSrc;
+            showDiv[index].style.display = "none";
+            clearInterval(up);
+        }
+        },50);
+   }
 }
